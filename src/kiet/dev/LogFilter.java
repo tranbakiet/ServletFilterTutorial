3cf2f692e6fbf02e76d3632f2863f0deb4bfe2f4
Kiet <206love193@gmail.com>
2018-10-04 23:40:37 +0700

    Finish logFilter

---
+++ src/kiet/dev/LogFilter.java
@@ -0,0 +1,43 @@
+package kiet.dev;
+
+import java.io.IOException;
+import java.util.Date;
+ 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+ 
+public class LogFilter implements Filter {
+ 
+    public LogFilter() {
+    }
+ 
+    @Override
+    public void init(FilterConfig fConfig) throws ServletException {
+        System.out.println("LogFilter init!");
+    }
+ 
+    @Override
+    public void destroy() {
+        System.out.println("LogFilter destroy!");
+    }
+ 
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+ 
+        HttpServletRequest req = (HttpServletRequest) request;
+ 
+        String servletPath = req.getServletPath();
+ 
+        System.out.println("#INFO " + new Date() + " - ServletPath :" + servletPath //
+                + ", URL =" + req.getRequestURL());
+ 
+        chain.doFilter(request, response);
+    }
+ 
+}
